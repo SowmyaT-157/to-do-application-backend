9149f4d86aac7e75c66d94162476dfda53b9ce7e
sowmya <sowmya.t@everest.engineering>
2025-11-18 18:26:38 +0530

    Update the routes for add delete method

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import {  addTheNewTask, getAllTasks } from "../controllers/taskController";
+import {  addTheNewTask, getAllTasks, removeTheTask } from "../controllers/taskController";
 
 
 export const router = Router()
 router.get('/getTasks',getAllTasks)
 router.post('/addTask',addTheNewTask)
+router.delete('/deleteTask/:id',removeTheTask )
