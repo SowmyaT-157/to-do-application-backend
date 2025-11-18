ba5c5432597ea3c87aabfa26abf086e59f1a92d5
sowmya <sowmya.t@everest.engineering>
2025-11-18 22:31:55 +0530

    Update the routes for add patch router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {  addTheNewTask, getAllTasks, removeTheTask } from "../controllers/taskController";
+import {  addTheNewTask, getAllTasks, removeTheTask, updateTheTask } from "../controllers/taskController";
 
 
 export const router = Router()
 router.get('/getTasks',getAllTasks)
 router.post('/addTask',addTheNewTask)
 router.delete('/deleteTask/:id',removeTheTask )
+router.patch('/updateTask/:id',updateTheTask)
